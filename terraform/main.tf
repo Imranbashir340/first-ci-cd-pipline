02dc16d96371e3ddc9431ffdb2f243b57f9c9fc9
imranbashir340 <imranbashir340@gcuf.edu.pk>
2025-07-02 06:33:04 -0700

    first commit

---
+++ terraform/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-key"
+  public_key = "SHA256:rt2jTr2OdeDxt5a0SWG2fIl/CzQ4q6yzc0g+JPsL9oM imran.bashir340@gmail.com" # Make sure key exists
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.deployer.key_name
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum update -y",
+      "sudo amazon-linux-extras install docker -y",
+      "sudo service docker start",
+      "sudo usermod -a -G docker ec2-user",
+      "docker run -d -p 80:80 ghcr.io/yourusername/yourimage:latest"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("~/.ssh/id_rsa")
+      host        = self.public_ip
+    }
+  }
+
+  tags = {
+    Name = "MyDockerWebServer"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
